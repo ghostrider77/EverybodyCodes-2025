a8bb9648264e739ac5f9b88afe9dbf6725c23924
Tamas Kurics <tamas.kurics@gmail.com>
2026-09-14 08:31:31 +0200

    fix typo in function name
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- src/quest03/quest.ml
+++ src/quest03/quest.ml
@@ -11,5 +11,5 @@ let find_sum_of_unique_elements crates =
   IntSet.fold (+) (IntSet.of_list crates) 0
 
 
-let find_sum_of_smallest_set_of_give_size crates k =
+let find_sum_of_smallest_set_of_given_size crates k =
   List.(crates |> sort_uniq Stdlib.compare |> take k |> fold_left (+) 0)
