d2c546944a21b5a8f2e462cc5b045939c3a7d36c
Tamas Kurics <tamas.kurics@gmail.com>
2026-09-14 05:33:39 +0200

    add a test case to quest 2
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- src/quest02/tests.ml
+++ src/quest02/tests.ml
@@ -9,6 +9,18 @@ let tests =
       let expected = "[357,862]" in
       assert_equal expected result ~printer:(Printf.sprintf "%S")
       );
+    "should calculate the result by repeating the steps 100 times on a reduced grid" >:: (fun _ ->
+      let top_left = Quest.ComplexNumber.init 35300 (-64910) in
+      let coord_grid = Quest.create_grid top_left in
+      let grid_numbers = Seq.map (fun (x, y) -> Quest.ComplexNumber.init x y) coord_grid in
+      let result =
+        Seq.fold_left
+          (fun acc n -> if Option.is_some (Quest.perform_conditional_cycles n 100) then acc + 1 else acc)
+          0
+          grid_numbers in
+      let expected = 4076 in
+      assert_equal expected result ~printer:(Printf.sprintf "%d")
+      );
   ]
 
 
