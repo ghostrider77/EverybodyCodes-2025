b3d72192a1ec976a358bb463116b92fef1348de4
Tamas Kurics <tamas.kurics@gmail.com>
2026-09-22 14:35:58 +0200

    add tests to quest 6
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- src/quest06/tests.ml
+++ src/quest06/tests.ml
@@ -10,6 +10,35 @@ let tests =
       let expected = 5 in
       assert_equal expected result ~printer:(Printf.sprintf "%d")
       );
+
+    "should calculate the mentor-novice pairs for a short input within a given radius without repeat" >:: (fun _ ->
+      let str = "AABCBABCABCabcabcABCCBAACBCa" in
+      let heroes = Quest.parse_input str in
+      let repeat = 1 in
+      let radius = 10 in
+      let result = Quest.count_nearby_mentor_novice_pairs heroes repeat radius in
+      let expected = 34 in
+      assert_equal expected result ~printer:(Printf.sprintf "%d")
+      );
+
+    "should calculate the mentor-novice pairs for a short input within a given radius with repeat" >:: (fun _ ->
+      let str = "AABCBABCABCabcabcABCCBAACBCa" in
+      let heroes = Quest.parse_input str in
+      let repeat = 2 in
+      let radius = 10 in
+      let result = Quest.count_nearby_mentor_novice_pairs heroes repeat radius in
+      let expected = 72 in
+      assert_equal expected result ~printer:(Printf.sprintf "%d")
+      );
+    "should calculate the mentor-novice pairs for a short input within a given radius with many repeats" >:: (fun _ ->
+      let str = "AABCBABCABCabcabcABCCBAACBCa" in
+      let heroes = Quest.parse_input str in
+      let repeat = 1000 in
+      let radius = 1000 in
+      let result = Quest.count_nearby_mentor_novice_pairs heroes repeat radius in
+      let expected = 3442321 in
+      assert_equal expected result ~printer:(Printf.sprintf "%d")
+      );
   ]
 
 
